4852ae4ccac14b3f1f079c3ef3b5b0f69da389fc
Aryan Pareek <apareek369@gmail.com>
2020-07-05 21:49:26 -0700

    Initial Commit

---
+++ SlotsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotsGame
+//
+//  Created by Aryan Pareek on 7/5/20.
+//  Copyright © 2020 Aryan Pareek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
